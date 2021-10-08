df3aae6ba5c12fcb5e5a84cbdad5abbdada32b72
Jules De Vil <jules.devil@hotmail.com>
2021-10-08 15:45:09 +0200

    func4 made

--- Session2.py
+++ Session2.py
@@ -1,6 +1,17 @@
 
 
 def func(a, b):
-    return a + b
+    return a + b + 2
 
-    
+
+def func2(a, b):
+    return a*b
+
+
+def func3 (a):
+    return a + 3
+
+
+def func4(a, b):
+    return a/b
+
